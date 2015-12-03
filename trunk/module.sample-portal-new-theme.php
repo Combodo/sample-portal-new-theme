603b7d43e0848c7349455f8cfded30927e348ccf
Denis Flaven <denis.flaven@combodo.com>
2015-12-03 10:18:09 +0000

    Samples for the new portal mechanism !
    SVN:sample-portal-new-theme[4919]

---
+++ trunk/module.sample-portal-new-theme.php
@@ -0,0 +1,52 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'sample-portal-new-theme/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Sample Portal (new theme)',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-portal'
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.sample-portal-new-theme.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
